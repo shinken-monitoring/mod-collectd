b93c37cb868373e6066b5eb1833e7850004e6418
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2014-02-27 17:08:25 -0500

    * Add dake module

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,6 +21,36 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../'))
 
+# Fix for missing modules
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['shinken',
+                'shinken.log',
+                'logger',
+                'shinken.basemodule',
+                'BaseModule',
+                'shinken.external_command',
+                'ExternalCommand',
+                ]
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
 
 # -- General configuration ------------------------------------------------
 
